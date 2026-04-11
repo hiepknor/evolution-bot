e0325c0bb9f70551d281bbdd58368efdb992a3f1
Hiep Mai <hiepknor@MacBook-Air-cua-Hiep.local>
2026-04-11 14:59:29 +0700

    chore(tauri): remove unused shell capability and refresh schemas

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ pub fn run() {
     .plugin(tauri_plugin_dialog::init())
     .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_http::init())
-    .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_sql::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
